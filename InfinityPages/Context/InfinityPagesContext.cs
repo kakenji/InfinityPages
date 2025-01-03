64ca7ef05a6ff10326a6384e8492ef52184d4d35
Kayky Kenji <kaykyjk01@gmail.com>
2025-01-03 16:01:36 -0300

    Added files

---
+++ InfinityPages/Context/InfinityPagesContext.cs
@@ -0,0 +1,13 @@
+﻿using InfinityPages.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfinityPages.Context
+{
+    public class InfinityPagesContext : DbContext
+    {
+        public DbSet<Book> BookStore { get; set; }
+        public InfinityPagesContext(DbContextOptions<InfinityPagesContext> options) : base(options) 
+        { 
+        }
+    }
+}
---
+++ InfinityPages/Controllers/InfinityPages.cs
@@ -0,0 +1,26 @@
+﻿using InfinityPages.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InfinityPages.Controllers
+{
+    public class InfinityPages : Controller
+    {
+        private InfinityPagesContext _context;
+        public InfinityPages(InfinityPagesContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Books()
+        {
+            var books = _context.BookStore.ToList();
+            return View(books);
+        }
+
+    }
+}
